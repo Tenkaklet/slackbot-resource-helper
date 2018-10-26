958bcd23a456d660d35d7db16946da066886c89b
Max Carlquist <tenkaklet@gmail.com>
2018-10-26 13:17:01 +0200

    added for home

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const SlackBot = require('slackbots');
 const request = require('request');
+const signale = require('signale');
 const axios = require('axios');
+
+
 
 const bot = new SlackBot({
     token: 'xoxb-416906710166-463631229057-TQclrJ2gD8fU6YeoZO6g7Ds9',
@@ -11,18 +14,31 @@ bot.on('start', () => {
     var params = {
         icon_emoji: ':success:'
     }
-    bot.postMessageToChannel('test', 'Get info', params);
+    bot.postMessageToChannel('test', getFromStackApi, params);
 });
 
-bot.on('error',  (err) => {
+bot.on('error', (err) => {
     console.log(err);
-}); 
+});
 
 // Send message
-bot.on('message',  (data) => {
+bot.on('message', (data) => {
     if (data.type != 'message') {
         return;
     }
-
-    console.log(data);
+    getFromStackApi(data.text);
 });
+
+// Respond to data
+// function sendReturnData(message) {
+//     signale.debug(message);
+//     return message;
+// }
+
+// get request data from Stackoverflow
+function getFromStackApi (query) {
+    axios.get(`http://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow&tagged=${query}`)
+    .then(function (res) {
+        signale.success('res -> ', res.data);
+    });
+}
